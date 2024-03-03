2588395d5830fa84eb18d51bf82088c464812627
KimYouGyoung <tptkds12@gmail.com>
2024-03-03 22:43:10 +0900

    Provider를  layout.tsx에 추가

--- src/app/StoreProvider.tsx
+++ src/app/StoreProvider.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { useRef } from 'react';
 import { Provider } from 'react-redux';
 import { makeStore } from '../store/index';
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import '../styles/globals.css';
+import StoreProvider from './StoreProvider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -15,8 +16,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <StoreProvider>
+      <html lang="ko">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </StoreProvider>
   );
 }
