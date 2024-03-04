ab78019d908bb5e156141f0e21acffb41ebad5d6
Finn <tptkds12@gmail.com>
2024-03-04 14:58:35 +0900

    ThemeProvider 수정

--- src/app/ThemeProvider.tsx
+++ src/app/ThemeProvider.tsx
@@ -1,9 +1,10 @@
 'use client';
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
+import { type ThemeProviderProps } from 'next-themes/dist/types';
 
-import { ThemeProvider } from 'next-themes';
-
-const Provider = ({ children }: { children: React.ReactNode }) => {
-  return <ThemeProvider attribute="class">{children}</ThemeProvider>;
-};
-
-export default ThemeProvider;
+export default function ThemeProvider({
+  children,
+  ...props
+}: ThemeProviderProps) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+}
