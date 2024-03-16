aa9cb9c306486a6ba3fd52577df1128655de2599
KimYouGyoung <tptkds12@gmail.com>
2024-03-16 20:52:16 +0900

    wishlist 로그인 상태에서만 사용할 수 있게 수정

--- src/app/wishlist/page.tsx
+++ src/app/wishlist/page.tsx
@@ -1,7 +1,25 @@
-import React from 'react';
+'use client';
+import React, { useEffect } from 'react';
 import List from './components/List';
+import { useRouter } from 'next/navigation';
+import { auth } from '../firebaseConfigure';
+import { Unsubscribe, onAuthStateChanged } from 'firebase/auth';
 
 export default function WishlistComponent() {
+  const router = useRouter();
+  useEffect(() => {
+    const unsubscribe: Unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (!user) {
+        alert('Login required service.');
+        router.push('/');
+      }
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, [router]);
+
   return (
     <div>
       <div className="mt-14 flex flex-col justify-center w-full items-center">
