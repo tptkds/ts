6faf396692050d2276cd45ea43b47c2cbf1a25f6
KimYouGyoung <tptkds12@gmail.com>
2024-03-17 16:36:37 +0900

    Footer 아이콘 사이즈 및 Link 추가

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,16 +1,32 @@
 import { FaGithub } from 'react-icons/fa';
 import { SiVelog } from 'react-icons/si';
 import React from 'react';
+import Link from 'next/link';
 
 export default function Footer() {
   return (
     <footer className=" px-4 sm:px-12 py-14 relative bg-white bg-opacity-60  flex flex-col items-center  shadow-md dark:bg-zinc-900 dark:text-white">
       <ul className="flex text-lg mb-4">
         <li className="mr-4">
-          <FaGithub />
+          <Link
+            href="https://github.com/tptkds/showfinnmore"
+            rel="noopener noreferrer"
+            target="_blank"
+            style={{ fontSize: '28px' }}
+          >
+            <FaGithub />
+          </Link>
         </li>
+
         <li>
-          <SiVelog />
+          <Link
+            href="https://velog.io/@wlldone/posts"
+            rel="noopener noreferrer"
+            target="_blank"
+            style={{ fontSize: '28px' }}
+          >
+            <SiVelog />
+          </Link>
         </li>
       </ul>
       <ul className="text-xs">
