89539afb4a36edbeca92130cf03e389cde0aadd9
Finn <tptkds12@gmail.com>
2024-03-18 15:22:00 +0900

    Product List 장바구니 버튼, 찜 버튼 코드 개선

--- src/app/product/[...slug]/components/CartButton.tsx
+++ src/app/product/[...slug]/components/CartButton.tsx
@@ -2,69 +2,54 @@
 import React, { useContext } from 'react';
 import { useAppDispatch } from '@/hooks/useAppDispatch';
 import { setCartItems } from '@/slices/productSlict';
-import { CartItems, Product } from '@/types/globalTypes';
+import { Product, CartItems } from '@/types/globalTypes';
+import { AuthContext } from '@/app/AuthProvider';
+import { doc, updateDoc } from 'firebase/firestore';
+import { db } from '@/app/firebaseConfig';
+import { PiShoppingBagFill, PiShoppingBagLight } from 'react-icons/pi';
 import {
-  getCartItemsLocalStorage,
   addCartItemsLocalStorage,
   deleteCartItemsLocalStorage,
 } from '@/utilities/localstorage';
-import { PiHeart, PiShoppingBagFill, PiShoppingBagLight } from 'react-icons/pi';
-import { AuthContext } from '@/app/AuthProvider';
-import { userInfo } from 'os';
-import { db } from '@/app/firebaseConfig';
-import { doc, updateDoc } from 'firebase/firestore';
 
-function CartButton({
-  product,
-  cartItems,
-}: {
+interface CartButtonProps {
   product: Product;
   cartItems: CartItems;
-}) {
-  const dispatch = useAppDispatch();
-  const keysInCart: string[] = Object.keys(cartItems);
-  const { currentUser } = useContext(AuthContext);
-  const toggleCartItem = (e: any) => {
-    e.stopPropagation();
+}
 
-    if (currentUser) {
-      if ([...keysInCart].includes(product.id.toString())) {
-        let newCartItems: CartItems = { ...cartItems };
-        delete newCartItems[product.id];
-        let userRef = null;
-        if (currentUser?.email) userRef = doc(db, 'users', currentUser?.email);
-        if (userRef)
-          updateDoc(userRef, {
-            cartItems: newCartItems,
-          }).then(() => {
-            dispatch(setCartItems(newCartItems));
-          });
+const CartButton: React.FC<CartButtonProps> = ({ product, cartItems }) => {
+  const { currentUser } = useContext(AuthContext);
+  const dispatch = useAppDispatch();
+
+  const toggleCartItem = async () => {
+    const productID = product.id.toString();
+    let newCartItems: CartItems = { ...cartItems };
+
+    if (currentUser && currentUser.email) {
+      // 로그인 사용자: Firestore 데이터 업데이트
+      if (newCartItems[productID]) {
+        delete newCartItems[productID];
       } else {
-        const newCartItems: CartItems = {
-          ...cartItems,
-          [product.id]: { product: product, count: 1 },
-        };
-        let userRef = null;
-        if (currentUser?.email) userRef = doc(db, 'users', currentUser?.email);
-        if (userRef)
-          updateDoc(userRef, {
-            cartItems: newCartItems,
-          }).then(() => {
-            dispatch(setCartItems(newCartItems));
-          });
+        newCartItems[productID] = { product, count: 1 };
       }
+
+      const userRef = doc(db, 'users', currentUser.email);
+      await updateDoc(userRef, { cartItems: newCartItems });
     } else {
-      if ([...keysInCart].includes(product.id.toString()))
-        deleteCartItemsLocalStorage([product.id.toString()]);
-      else addCartItemsLocalStorage(product);
-      const newCartItems: CartItems | undefined = getCartItemsLocalStorage();
-      if (newCartItems !== undefined) dispatch(setCartItems(newCartItems));
-      else dispatch(setCartItems({}));
+      if (newCartItems[productID]) {
+        delete newCartItems[productID];
+        deleteCartItemsLocalStorage([productID]);
+      } else {
+        newCartItems[productID] = { product, count: 1 };
+        addCartItemsLocalStorage(product);
+      }
     }
+
+    dispatch(setCartItems(newCartItems));
   };
 
   return (
-    <button type="button" onClick={toggleCartItem}>
+    <button onClick={toggleCartItem}>
       {cartItems[product.id] ? (
         <PiShoppingBagFill style={{ fontSize: '28px' }} />
       ) : (
@@ -72,6 +57,82 @@ function CartButton({
       )}
     </button>
   );
-}
+};
 
 export default CartButton;
+// import React, { useContext } from 'react';
+// import { useAppDispatch } from '@/hooks/useAppDispatch';
+// import { setCartItems } from '@/slices/productSlict';
+// import { CartItems, Product } from '@/types/globalTypes';
+// import {
+//   getCartItemsLocalStorage,
+//   addCartItemsLocalStorage,
+//   deleteCartItemsLocalStorage,
+// } from '@/utilities/localstorage';
+// import { PiHeart, PiShoppingBagFill, PiShoppingBagLight } from 'react-icons/pi';
+// import { AuthContext } from '@/app/AuthProvider';
+// import { userInfo } from 'os';
+// import { db } from '@/app/firebaseConfig';
+// import { doc, updateDoc } from 'firebase/firestore';
+
+// function CartButton({
+//   product,
+//   cartItems,
+// }: {
+//   product: Product;
+//   cartItems: CartItems;
+// }) {
+//   const dispatch = useAppDispatch();
+//   const keysInCart: string[] = Object.keys(cartItems);
+//   const { currentUser } = useContext(AuthContext);
+//   const toggleCartItem = (e: any) => {
+//     e.stopPropagation();
+
+//     if (currentUser) {
+//       if ([...keysInCart].includes(product.id.toString())) {
+//         let newCartItems: CartItems = { ...cartItems };
+//         delete newCartItems[product.id];
+//         let userRef = null;
+//         if (currentUser?.email) userRef = doc(db, 'users', currentUser?.email);
+//         if (userRef)
+//           updateDoc(userRef, {
+//             cartItems: newCartItems,
+//           }).then(() => {
+//             dispatch(setCartItems(newCartItems));
+//           });
+//       } else {
+//         const newCartItems: CartItems = {
+//           ...cartItems,
+//           [product.id]: { product: product, count: 1 },
+//         };
+//         let userRef = null;
+//         if (currentUser?.email) userRef = doc(db, 'users', currentUser?.email);
+//         if (userRef)
+//           updateDoc(userRef, {
+//             cartItems: newCartItems,
+//           }).then(() => {
+//             dispatch(setCartItems(newCartItems));
+//           });
+//       }
+//     } else {
+//       if ([...keysInCart].includes(product.id.toString()))
+//         deleteCartItemsLocalStorage([product.id.toString()]);
+//       else addCartItemsLocalStorage(product);
+//       const newCartItems: CartItems | undefined = getCartItemsLocalStorage();
+//       if (newCartItems !== undefined) dispatch(setCartItems(newCartItems));
+//       else dispatch(setCartItems({}));
+//     }
+//   };
+
+//   return (
+//     <button type="button" onClick={toggleCartItem}>
+//       {cartItems[product.id] ? (
+//         <PiShoppingBagFill style={{ fontSize: '28px' }} />
+//       ) : (
+//         <PiShoppingBagLight style={{ fontSize: '28px' }} />
+//       )}
+//     </button>
+//   );
+// }
+
+// export default CartButton;
--- src/app/product/[...slug]/components/WishlistButton.tsx
+++ src/app/product/[...slug]/components/WishlistButton.tsx
@@ -1,56 +1,48 @@
 'use client';
 import React, { useContext } from 'react';
 import { useAppDispatch } from '@/hooks/useAppDispatch';
-import { Product, Wishlist } from '@/types/globalTypes';
-import { PiHeartFill, PiHeartLight } from 'react-icons/pi';
 import { setWishlist } from '@/slices/productSlict';
+import { Product, Wishlist } from '@/types/globalTypes';
+import { AuthContext } from '@/app/AuthProvider';
 import { doc, updateDoc } from 'firebase/firestore';
 import { db } from '@/app/firebaseConfig';
-import { AuthContext } from '@/app/AuthProvider';
+import { PiHeartFill, PiHeartLight } from 'react-icons/pi';
 
-export default function WishlistButton({
-  product,
-  wishlist,
-}: {
+interface WishlistButtonProps {
   product: Product;
   wishlist: Wishlist;
-}) {
+}
+
+const WishlistButton: React.FC<WishlistButtonProps> = ({
+  product,
+  wishlist,
+}) => {
   const { currentUser } = useContext(AuthContext);
   const dispatch = useAppDispatch();
-  const keysInCWishlist: string[] = Object.keys(wishlist);
 
-  const toggleWishlist = (e: any) => {
-    e.stopPropagation();
-    if (!currentUser) {
+  const toggleWishlist = async () => {
+    if (!currentUser || !currentUser.email) {
       alert('로그인이 필요한 기능입니다.');
       return;
     }
-    if ([...keysInCWishlist].includes(product.id.toString())) {
-      let newWishlist: Wishlist = { ...wishlist };
-      delete newWishlist[product.id];
-      let userRef = null;
-      if (currentUser?.email) userRef = doc(db, 'users', currentUser?.email);
-      if (userRef)
-        updateDoc(userRef, {
-          wishlist: newWishlist,
-        }).then(() => {
-          dispatch(setWishlist(newWishlist));
-        });
+
+    const newWishlist = { ...wishlist };
+    const productID = product.id.toString();
+
+    if (wishlist[productID]) {
+      delete newWishlist[productID];
     } else {
-      const newWishlist: Wishlist = { ...wishlist, [product.id]: product };
-      let userRef = null;
-      if (currentUser?.email) userRef = doc(db, 'users', currentUser?.email);
-      if (userRef)
-        updateDoc(userRef, {
-          wishlist: newWishlist,
-        }).then(() => {
-          dispatch(setWishlist(newWishlist));
-        });
+      newWishlist[productID] = product;
     }
+
+    const userRef = doc(db, 'users', currentUser.email);
+    await updateDoc(userRef, { wishlist: newWishlist });
+
+    dispatch(setWishlist(newWishlist));
   };
 
   return (
-    <button type="button" onClick={toggleWishlist}>
+    <button onClick={toggleWishlist}>
       {wishlist[product.id] ? (
         <PiHeartFill style={{ fontSize: '28px' }} />
       ) : (
@@ -58,4 +50,67 @@ export default function WishlistButton({
       )}
     </button>
   );
-}
+};
+
+export default WishlistButton;
+
+// import React, { useContext } from 'react';
+// import { useAppDispatch } from '@/hooks/useAppDispatch';
+// import { Product, Wishlist } from '@/types/globalTypes';
+// import { PiHeartFill, PiHeartLight } from 'react-icons/pi';
+// import { setWishlist } from '@/slices/productSlict';
+// import { doc, updateDoc } from 'firebase/firestore';
+// import { db } from '@/app/firebaseConfig';
+// import { AuthContext } from '@/app/AuthProvider';
+
+// export default function WishlistButton({
+//   product,
+//   wishlist,
+// }: {
+//   product: Product;
+//   wishlist: Wishlist;
+// }) {
+//   const { currentUser } = useContext(AuthContext);
+//   const dispatch = useAppDispatch();
+//   const keysInCWishlist: string[] = Object.keys(wishlist);
+
+//   const toggleWishlist = (e: any) => {
+//     e.stopPropagation();
+//     if (!currentUser) {
+//       alert('로그인이 필요한 기능입니다.');
+//       return;
+//     }
+//     if ([...keysInCWishlist].includes(product.id.toString())) {
+//       let newWishlist: Wishlist = { ...wishlist };
+//       delete newWishlist[product.id];
+//       let userRef = null;
+//       if (currentUser?.email) userRef = doc(db, 'users', currentUser?.email);
+//       if (userRef)
+//         updateDoc(userRef, {
+//           wishlist: newWishlist,
+//         }).then(() => {
+//           dispatch(setWishlist(newWishlist));
+//         });
+//     } else {
+//       const newWishlist: Wishlist = { ...wishlist, [product.id]: product };
+//       let userRef = null;
+//       if (currentUser?.email) userRef = doc(db, 'users', currentUser?.email);
+//       if (userRef)
+//         updateDoc(userRef, {
+//           wishlist: newWishlist,
+//         }).then(() => {
+//           dispatch(setWishlist(newWishlist));
+//         });
+//     }
+//   };
+
+//   return (
+//     <button type="button" onClick={toggleWishlist}>
+//       {wishlist[product.id] ? (
+//         <PiHeartFill style={{ fontSize: '28px' }} />
+//       ) : (
+//         <PiHeartLight style={{ fontSize: '28px' }} />
+//       )}
+//     </button>
+//   );
+// }
