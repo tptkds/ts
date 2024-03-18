ff8dcd1ba5932e34bd5a2e18ebc61c801f91a35b
Finn <tptkds12@gmail.com>
2024-03-18 16:12:53 +0900

    ProductList 스켈레톤 적용

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,13 +1,34 @@
+'use client';
 import React from 'react';
 import Skeleton from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
+import { usePathname } from 'next/navigation';
 
 export default function Loading() {
-  const productSkeleton = <></>;
+  const productListSkeleton = (
+    <ul className="mt-28 grid grid-cols-1 gap-8 min-h-screen sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      {Array.from({ length: 12 }).map((_, index) => (
+        <li key={index} className="relative h-full animate-pulse pb-24">
+          <div className="h-4/5 mb-4">
+            <Skeleton height="100%" />
+          </div>
+          <div>
+            <Skeleton height={20} width={`60%`} />
+          </div>
+          <div className="mt-2 mb-24">
+            <Skeleton height={20} width={`30%`} />
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+  const pathname = usePathname();
+
   return (
-    <div className="h-svh pt-24 ">
-      <Skeleton height={30} width={`60%`} /> {/* 예시로 제목용 스켈레톤 */}
-      <Skeleton count={5} height={10} width={`80%`} />{' '}
-    </div>
+    <>
+      {pathname.startsWith('/product/') && (
+        <div className="min-h-screen">{productListSkeleton}</div>
+      )}
+    </>
   );
 }
